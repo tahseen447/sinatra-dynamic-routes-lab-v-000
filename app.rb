b4486934d42158aa6011f32c0cd27109e4c9e8d8
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-07 05:10:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,8 +23,8 @@ class App < Sinatra::Base
     word1=params[:word1]
     word2 =params[:word2]
     word3=params[:word3]
-    word1=params[:word1]
-    word1=params[:word1]
+    word4=params[:word4]
+    word5=params[:word5]
   end
 
 end
