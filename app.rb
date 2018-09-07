7e62215acd125806ecd472109485bbed12d3c7aa
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-07 05:17:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -36,8 +36,11 @@ class App < Sinatra::Base
     if operation == "add"
       "#{number1 + number2}"
     elsif operation == "subtract"
+      "#{number1 + number2}"
     elsif operation == 'multiply'
+      "#{number1 + number2}"
     elsif operation == 'divide'
+      "#{number1 + number2}"
     end
   end
 
