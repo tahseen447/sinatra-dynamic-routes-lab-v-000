9f891d92fba5ad09a6b5f5d56a823c1c1269a5d9
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-07 05:13:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,6 +31,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     operation = params[:operation]
     number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
   end
 
 end
